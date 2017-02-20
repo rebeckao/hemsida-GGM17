fc7b83c0973ab9d764e93e311ce7c2e395056617
rebeckao <rof@itello.se>
2017-02-20 13:43:03 +0100

    Fixade indenteringen

--- index.php
+++ index.php
@@ -8,20 +8,21 @@
 <?php include("includes/header.html"); ?>
 <!-- content -->
 <div class="wrapper row3">
-  <div id="container">
-    <!-- ################################################################################################ -->
-    <div id="homepage" class="clear">
-      <div class="four_fifth first" id="intro">
-        <!-- #### -->
-        <section class="">
+	<div id="container">
+		<!-- ################################################################################################ -->
+		<div id="homepage" class="clear">
+			<div class="four_fifth first" id="intro">
+				<!-- #### -->
+				<section class="">
 					<h1>Geek Evolution</h1>
-          <div class="two_third first">
-            <p>En beskrivning av årets tema och hur awesome det är! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam blandit justo eget felis condimentum pretium. Suspendisse eget est aliquam, porta libero fermentum, elementum ipsum. Morbi congue vel tortor non condimentum.</p>
-          </div>
+					<div class="two_third first">
+						<p>En beskrivning av årets tema och hur awesome det är! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam blandit justo eget felis condimentum pretium. Suspendisse eget est aliquam, porta libero fermentum, elementum ipsum.</p>
+						<p>Morbi congue vel tortor non condimentum.</p>
+					</div>
 					<div class="one_third"><a href="https://simplesignup.se/event/74734-geek-girl-meetup-riksevent-ggm16-better-together" class="button large gradient orange">Anmäl dig nu</a></div>
-        </section>
-        <!-- #### -->
-        <section id="notiser">
+				</section>
+				<!-- #### -->
+				<section id="notiser">
 					<ul class="nospace push50 clear">
 						<li class="one_third first">
 							<div class="push20"><a href="info/info.php"><img src="images/demo/gallery.gif" alt="Praktisk information"></a></div>
@@ -34,42 +35,40 @@
 						</li>
 					</ul>
 				</section>
-        <!-- #### -->
-      </div>
-      <div class="one_fifth">
-		<section id="sponsorer">
-			<figure>
-				<h2 class="font-medium"><a href="#">Sponsorer</a></h2>
-				<ul class="nospace spacing clear">
-					<li>
-						<a href="http://www.acando.se/" target="_blank">
-							<img src="images/sponsorer/Acando_Tagline_Blue_RGB_Office.png" alt="Acando"/>
-						</a>
-					</li>
-					<li>
-						<a href="https://www.academicwork.se/" target="_blank">
-							<img src="images/sponsorer/akademicwork_logo.png" alt="Academic Work"/>
-						</a>
-					</li>
-					<li>
-						<a href="http://www.mirado.com/" target="_blank">
-							<img src="images/sponsorer/Mirado.jpg" alt="Mirado"/>
-						</a>
-					</li>
-					<li>
-						<a href="https://www.spotify.com/se/" target="_blank">
-							<img src="images/sponsorer/spotify_logo.png" alt="Spotify"/>
-						</a>
-					</li>
-				</ul>
-				<figcaption>
-					<p align="right"><a href="delta/delta.php#bli_sponsor">Bli sponsor</a></p>
-				</figcaption>
-			</figure>
-		</section>
-      </div>
-    </div>
-  </div>
+				<!-- #### -->
+			</div>
+			<div class="one_fifth">
+				<section id="sponsorer">
+				<figure>
+					<h2 class="font-medium"><a href="#">Sponsorer</a></h2>
+					<ul class="nospace spacing clear">
+						<li>
+							<a href="http://www.acando.se/" target="_blank">
+								<img src="images/sponsorer/Acando_Tagline_Blue_RGB_Office.png" alt="Acando"/>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.academicwork.se/" target="_blank">
+								<img src="images/sponsorer/akademicwork_logo.png" alt="Academic Work"/>
+							</a>
+						</li>
+						<li>
+							<a href="http://www.mirado.com/" target="_blank">
+								<img src="images/sponsorer/Mirado.jpg" alt="Mirado"/>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.spotify.com/se/" target="_blank">
+								<img src="images/sponsorer/spotify_logo.png" alt="Spotify"/>
+							</a>
+						</li>
+					</ul>
+					<figcaption><p align="right"><a href="delta/delta.php#bli_sponsor">Bli sponsor</a></p></figcaption>
+				</figure>
+				</section>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php include("includes/footer.html"); ?>
