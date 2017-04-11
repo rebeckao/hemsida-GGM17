eca0a453b2b9081351a33f2ba8d7b8580c184b8d
rebeckao <rebecka.oefverholm@gmail.com>
2017-04-11 21:34:08 +0200

    Uppdaterade texten för Maria Kareliusson

--- geeks/mariakareliusson.php
+++ geeks/mariakareliusson.php
@@ -13,11 +13,18 @@
   <section id="talare" class="darkgrayish top-of-page">
     <div class="row feature responsive">
       <div class="six columns left">
-        <img src="../images/geeks/Maria-Kareliusson.jpg" alt="Maria Kareliusson" />
+        <img src="../images/geeks/kareliusson_square.jpg" alt="Maria Kareliusson" />
+        <aside class="pull-quote">
+    			   <blockquote>
+    				   <p>Artificiell intelligens och mänsklig kognition kommer behöva förenas och vi kommer att se en förgrening i vad vi idag ser som en människa, cyborg-eran är här.</p>
+    				</blockquote>
+    			</aside>
       </div>
       <div class="six columns right">
         <h3>Maria Kareliusson</h3>
         <p>Maria Kareliusson är lifehacker och tidigare neurovetenskapsforskare med en passion för design som hjälper människor i vardagen. Hon brukar hålla demos i neurofeedback och är en del av BioNyfiken – en förening för biohackers, gör-det- själv-biologer, medborgarforskare och quantified self-entusiaster.</p>
+        <h4>TALK: Hack yourself – biohacking och mänsklig evolution</h4>
+        <p>Vad är biohacking och hur ser mänsklig evolution ut när vi integrerar teknik i kroppen?</p>
       </div>
     </div>
   </section>
