a74ce309f772061efddb4c4621e32546686de5e6
rebeckao <rof@itello.se>
2017-04-19 12:14:26 +0200

    Peka om separata talarsidor till huvudsidan

--- geeks/mariakareliusson.php
+++ geeks/mariakareliusson.php
@@ -1,35 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="sv"> <!--<![endif]-->
-<head>
-	<title>GGM17 - Maria Kareliusson</title>
-  <?php include '../includes/head.html';?>
-</head>
-
-<body>
-  <?php include '../includes/nav.php';?>
-  <section id="talare" class="darkgrayish top-of-page">
-    <div class="row feature responsive">
-      <div class="six columns left">
-        <img src="../images/geeks/kareliusson_square.jpg" alt="Maria Kareliusson" />
-        <aside class="pull-quote">
-    			   <blockquote>
-    				   <p>Artificiell intelligens och mänsklig kognition kommer behöva förenas och vi kommer att se en förgrening i vad vi idag ser som en människa, cyborg-eran är här.</p>
-    				</blockquote>
-    			</aside>
-      </div>
-      <div class="six columns right">
-        <h3>Maria Kareliusson</h3>
-        <p>Maria Kareliusson är lifehacker och tidigare neurovetenskapsforskare med en passion för design som hjälper människor i vardagen. Hon brukar hålla demos i neurofeedback och är en del av BioNyfiken – en förening för biohackers, gör-det- själv-biologer, medborgarforskare och quantified self-entusiaster.</p>
-        <h4>TALK: Hack yourself – biohacking och mänsklig evolution</h4>
-        <p>Vad är biohacking och hur ser mänsklig evolution ut när vi integrerar teknik i kroppen?</p>
-      </div>
-    </div>
-  </section>
-  <?php include '../includes/short-footer.html';?>
-  <?php include '../includes/scripts.html';?>
-</body>
-
-</html>
+<?php
+header("Location: http://www.geekevolution.se/#talare");
+exit;
+?>
--- geeks/siduripoli.php
+++ geeks/siduripoli.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="sv"> <!--<![endif]-->
-<head>
-	<title>GGM17 - Siduri Poli</title>
-  <?php include '../includes/head.html';?>
-</head>
-
-<body>
-  <?php include '../includes/nav.php';?>
-  <section id="talare" class="darkgrayish top-of-page">
-    <div class="row feature responsive">
-      <div class="six columns left">
-        <img src="../images/geeks/Siduri-Poli.jpg" alt="Siduri Poli" />
-      </div>
-      <div class="six columns right">
-        <h3>Siduri Poli</h3>
-        <p>Siduri Poli har en bakgrund som journalist och är teknikexpert för några av Sveriges största medier. Hon är medgrundare till demokratiseringsinitiativet Changers Hub som hjälper unga vuxna i Stockholms ytterstad att förverkliga sina idéer, och brinner för digitalisering, entreprenörskap och mångfald. Utsedd till en av Sveriges Supertalanger 2017 av Veckans Affärer.</p>
-      </div>
-    </div>
-  </section>
-  <?php include '../includes/short-footer.html';?>
-  <?php include '../includes/scripts.html';?>
-</body>
-
-</html>
+<?php
+header("Location: http://www.geekevolution.se/#talare");
+exit;
+?>
