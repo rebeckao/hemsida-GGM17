9a54212c3f88606ab78ef023521d3b2c6eb207b2
rebeckao <rebecka.oefverholm@gmail.com>
2017-05-07 16:25:42 +0200

    Sätt språk till svenska

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="sv"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="sv"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js ie ie9" lang="sv"> <![endif]-->
 <!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="sv"> <!--<![endif]-->
 <head>
 	<title>GGM17 - Geek Evolution</title>
