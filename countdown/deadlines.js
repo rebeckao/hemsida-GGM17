ad40d0bfddab08acdc08148b99cb2a724a95fec9
rebeckao <rebecka.oefverholm@gmail.com>
2017-05-07 15:36:39 +0200

    Fyllde i programpunkterna för nedräknaren

--- countdown/deadlines.js
+++ countdown/deadlines.js
@@ -6,9 +6,46 @@ deadlines = [
 			{itemName: "Immersiv teknologi – från förändring till förnyelse", speaker: "Cecilia Beck-Friis", place: "Verkstaden"},
 			{itemName: "Growth by making technology", speaker: "Nina Lindström", place: "Auditoriet"}
 	]},
+	{dateTime: new Date("May 13, 2017 11:00:00").getTime(), items: [{itemName: "Pitcha ditt företag till geek girls!", speaker: "Lisa Evertsson Norrevik", place: "Lobbyn"}]},
+	{dateTime: new Date("May 13, 2017 11:05:00").getTime(), items: [{itemName: "Videoteknik bakom kulisserna - utvecklingen och möjligheterna", speaker: "Isabelle Jouper & Marilia Bognandi", place: "Auditoriet"}]},
+	{dateTime: new Date("May 13, 2017 11:30:00").getTime(), items: [{itemName: "TBA", speaker: "Siren Hofvander", place: "Verkstaden"}]},
+	{dateTime: new Date("May 13, 2017 11:30:00").getTime(), items: [
+			{itemName: "TBA", speaker: "Siren Hofvander", place: "Verkstaden"},
+			{itemName: "Why you will never call your grandma from Mars and other news from computer science", speaker: "Nataliia Nuzhdina", place: "Auditoriet"}
+	]},
+	{dateTime: new Date("May 13, 2017 11:50:00").getTime(), items: [{itemName: "Genus i entreprenörsskap", speaker: "Olivia Whitehouse", place: "Auditoriet"}]},
+	{dateTime: new Date("May 13, 2017 12:00:00").getTime(), items: [{itemName: "Lunch", place: "Verkstaden"}]},
+	{dateTime: new Date("May 13, 2017 13:00:00").getTime(), items: [
+			{itemName: "Framtiden för live-video", speaker: "Annelie Demred", place: "Verkstaden"}
+			{itemName: "Hack yourself – biohacking & mänsklig evolution", speaker: "Maria Kareliusson", place: "Auditoriet"},
+	]},
+	{dateTime: new Date("May 13, 2017 13:15:00").getTime(), items: [{itemName: "Från utbränd till entreprenör", speaker: "Emma Hällström", place: "Lobbyn"}]},
+	{dateTime: new Date("May 13, 2017 13:35:00").getTime(), items: [{itemName: "TBA", place: "Lobbyn"}]},
+	{dateTime: new Date("May 13, 2017 13:45:00").getTime(), items: [
+			{itemName: "AI och framtiden", speaker: "Anne Håkansson", place: "Verkstaden"}
+			{itemName: "Framtidens arbetssätt i sjukvården", speaker: "Sara Lei", place: "Auditoriet"},
+	]},
+	{dateTime: new Date("May 13, 2017 14:05:00").getTime(), items: [{itemName: "Om världen varit jämställd hade jag hållit det här talet på Mars", speaker: "Maria Idebro", place: "Auditoriet"}]},
+	{dateTime: new Date("May 13, 2017 14:10:00").getTime(), items: [{itemName: "Robotik inom vård och omsorg", speaker: "Catharina Borgenstierna", place: "Lobbyn"}]},	
 	{dateTime: new Date("May 13, 2017 14:30:00").getTime(), items: [
 			{itemName: "Så skapar vi en inkluderande framtid", speaker: "Siduri Poli", place: "Verkstaden"},
 			{itemName: "Kina - världens hetaste VR-marknad", speaker: "Eva Kindlundh", place: "Auditoriet"},
 			{itemName: "We build the future", speaker: "Geek Girl Mini Sundsvall Crew", place: "Lobbyn"}
+	]},
+	{dateTime: new Date("May 13, 2017 14:50:00").getTime(), items: [{itemName: "Teknologi för hjärta och hjärna", speaker: "Mimmi Torneus", place: "Auditoriet"}]},	
+	{dateTime: new Date("May 13, 2017 15:00:00").getTime(), items: [
+			{itemName: "Fika", place: "Lobbyn"}
+			{itemName: "Workshop: Skapa med kreativ teknik", speaker: "Nina Lindström", place: "Mötesrum nära lobbyn"},
+	]},
+	{dateTime: new Date("May 13, 2017 15:15:00").getTime(), items: [{itemName: "TBA", speaker: "Anita Sarkeesian", place: "Verkstaden"}]},	
+	{dateTime: new Date("May 13, 2017 15:30:00").getTime(), items: [{itemName: "TBA", place: "Auditoriet"}]},	
+	{dateTime: new Date("May 13, 2017 15:50:00").getTime(), items: [{itemName: "TBA", place: "Auditoriet"}]},
+	{dateTime: new Date("May 13, 2017 16:00:00").getTime(), items: [{itemName: "Prisutdelning Elisabeth Tegner-stipendiet", speaker: "Emelie Fågelstedt & Elisabeth Tegner", place: "Verkstaden"}]},		
+	{dateTime: new Date("May 13, 2017 16:10:00").getTime(), items: [{itemName: "TBA", place: "Auditoriet"}]},
+	{dateTime: new Date("May 13, 2017 16:30:00").getTime(), items: [{itemName: "Sammanfattning", place: "Verkstaden"}]},
+	{dateTime: new Date("May 13, 2017 17:00:00").getTime(), items: [{itemName: "Middag", place: "Verkstaden"}]},
+	{dateTime: new Date("May 13, 2017 15:00:00").getTime(), items: [
+			{itemName: "Geek Evolution-Quiz!!!", speaker: "Copylinda & Kafkaspice", place: "Verkstaden"}
+			{itemName: "Fest/mingel"},
 	]}
 ]
