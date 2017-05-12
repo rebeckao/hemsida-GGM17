1658f2ac225e652c867ca49e0aa8af8c38b5c303
rebeckao <rebecka.oefverholm@gmail.com>
2017-05-12 18:37:58 +0200

    Kanske klar med designen av pausskärmarna

--- countdown/deadlines_rooms.js
+++ countdown/deadlines_rooms.js
@@ -1,22 +1,20 @@
 deadlines_rooms = [
 	[
-		//{dateTime: new Date("May 12, 2017 09:00:00").getTime(), itemName: "Registrering & frukost"},
-		//{dateTime: new Date("May 12, 2017 09:30:00").getTime(), itemName: "Välkomstpepp"},
-		{dateTime: new Date("May 12, 2017 10:00:00").getTime(), itemName: "Exponential Technologies", speaker: "Claudia Olsson"},
-		{dateTime: new Date("May 12, 2017 10:45:00").getTime(), itemName: "Immersiv teknologi – från förändring till förnyelse", speaker: "Cecilia Beck-Friis"},
-		{dateTime: new Date("May 12, 2017 11:30:00").getTime(), itemName: "Säkerhet för alla", speaker: "Siren Hofvander"},
-		{dateTime: new Date("May 12, 2017 12:00:00").getTime(), itemName: "Lunch"}
+		{dateTime: new Date("May 13, 2017 09:30:00").getTime(), itemName: "Välkomstpepp"},
+		{dateTime: new Date("May 13, 2017 10:00:00").getTime(), itemName: "Exponential Technologies", speaker: "Claudia Olsson"},
+		{dateTime: new Date("May 13, 2017 10:45:00").getTime(), itemName: "Immersiv teknologi – från förändring till förnyelse", speaker: "Cecilia Beck-Friis"},
+		{dateTime: new Date("May 13, 2017 11:30:00").getTime(), itemName: "Säkerhet för alla", speaker: "Siren Hofvander"},
+		{dateTime: new Date("May 13, 2017 12:00:00").getTime(), itemName: "Lunch"}
 	],
 	[
-		//{dateTime: new Date("May 12, 2017 09:00:00").getTime(), itemName: "Registrering & frukost"},
-		{dateTime: new Date("May 12, 2017 10:45:00").getTime(), itemName: "Growth by making technology", speaker: "Nina Lindström"},
-		{dateTime: new Date("May 12, 2017 11:05:00").getTime(), itemName: "Videoteknik bakom kulisserna - utvecklingen och möjligheterna", speaker: "Isabelle Jouper & Marilia Bognandi"},
-		{dateTime: new Date("May 12, 2017 11:30:00").getTime(), itemName: "Why you will never call your grandma from Mars and other news from computer science", speaker: "Nataliia Nuzhdina"},
-		{dateTime: new Date("May 12, 2017 11:50:00").getTime(), itemName: "Genus i entreprenörskap", speaker: "Olivia Whitehouse"},
-		{dateTime: new Date("May 12, 2017 12:40:00").getTime(), itemName: "Bli en gig:are du också!", speaker: "Marianne Olsson"}
+		{dateTime: new Date("May 13, 2017 10:45:00").getTime(), itemName: "Growth by making technology", speaker: "Nina Lindström"},
+		{dateTime: new Date("May 13, 2017 11:05:00").getTime(), itemName: "Videoteknik bakom kulisserna - utvecklingen och möjligheterna", speaker: "Isabelle Jouper & Marilia Bognandi"},
+		{dateTime: new Date("May 13, 2017 11:30:00").getTime(), itemName: "Why you will never call your grandma from Mars...", speaker: "Nataliia Nuzhdina"},
+		{dateTime: new Date("May 13, 2017 11:50:00").getTime(), itemName: "Genus i entreprenörskap", speaker: "Olivia Whitehouse"},
+		{dateTime: new Date("May 13, 2017 12:40:00").getTime(), itemName: "Bli en gig:are du också!", speaker: "Marianne Olsson"}
 	],
 	[
-		//{dateTime: new Date("May 12, 2017 09:00:00").getTime(), itemName: "Registrering & frukost"},
-		{dateTime: new Date("May 12, 2017 11:00:00").getTime(), itemName: "Pitcha ditt företag till geek girls!", speaker: "Lisa Evertsson Norrevik"}
+		{dateTime: new Date("May 13, 2017 09:00:00").getTime(), itemName: "Registrering & frukost"},
+		{dateTime: new Date("May 13, 2017 11:00:00").getTime(), itemName: "Pitcha ditt företag till geek girls!", speaker: "Lisa Evertsson Norrevik"}
 	]
 ]
